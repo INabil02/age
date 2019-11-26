3f762a942ec4881bacc476c02af350720b78bc43
NABIL Ismail <ismail@DomCIMR.MA>
2019-11-26 09:27:39 +0100

    first-commit-in-other-repo

---
+++ src/main/java/ma/ismail/AmazonSES/Test.java
@@ -0,0 +1,13 @@
+package ma.ismail.AmazonSES;
+
+public class Test {
+	public static void main(String[] args) {
+		Integer a = 5;
+		Integer b = 5;
+		
+		Integer c = 5;
+		
+		System.out.println(a == c);
+	}
+
+}
