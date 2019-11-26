fbaca2ef3337eef70253136cbe92e13f063452a7
ismail <ismail@Nabil-ismail.DomCIMR.MA>
2019-11-26 15:38:59 +0100

    example with difference between equals and ==

--- src/main/java/ma/ismail/AmazonSES/Test.java
+++ src/main/java/ma/ismail/AmazonSES/Test.java
@@ -2,12 +2,23 @@ package ma.ismail.AmazonSES;
 
 public class Test {
 	public static void main(String[] args) {
-		Integer a = 5;
-		Integer b = 5;
-		
-		Integer c = 5;
-		
-		System.out.println(a.equals(c));
+		String personalLoan = new String("cheap personal loans");
+		String homeLoan = new String("cheap personal loans");
+		     
+		//since two strings are different object result should be false
+		boolean result = personalLoan == homeLoan;
+		System.out.println("Comparing two strings with == operator: " + result);
+		     
+		//since strings contains same content , equals() should return true
+		result = personalLoan.equals(homeLoan);
+		System.out.println("Comparing two Strings with same content using equals method: " + result);
+		     
+		homeLoan = personalLoan;
+		//since both homeLoan and personalLoand reference variable are pointing to same object
+		//"==" should return true
+		result = (personalLoan == homeLoan);
+		System.out.println("Comparing two reference pointing to same String with == operator: " + result);
+
 	}
 
 }
