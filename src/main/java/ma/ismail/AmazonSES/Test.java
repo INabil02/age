d79b5a5babd7123a9ffcc14d51c5987f3766ac15
ismail <ismail@Nabil-ismail.DomCIMR.MA>
2019-11-29 15:15:43 +0100

    add Java_Cryptographic_Extensions

--- src/main/java/ma/ismail/AmazonSES/Test.java
+++ src/main/java/ma/ismail/AmazonSES/Test.java
@@ -1,7 +1,12 @@
 package ma.ismail.AmazonSES;
 
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
 public class Test {
-	public static void main(String[] args) {
+	
+	public void compareEqualsWithOperator() {
 		String personalLoan = new String("cheap personal loans");
 		String homeLoan = new String("cheap personal loans");
 		     
@@ -18,7 +23,39 @@ public class Test {
 		//"==" should return true
 		result = (personalLoan == homeLoan);
 		System.out.println("Comparing two reference pointing to same String with == operator: " + result);
-
+	}
+	
+	@SuppressWarnings("restriction")
+	public static String decryptage(String str) throws Exception{
+        SecretKey key = new SecretKeySpec("P@ssroot$/240204".getBytes(), "AES");
+		Cipher dcipher = Cipher.getInstance("AES");
+		dcipher.init(Cipher.DECRYPT_MODE, key);
+		byte[] dec= new sun.misc.BASE64Decoder().decodeBuffer(str);
+        byte[] utf8 = dcipher.doFinal(dec);
+        return new String(utf8, "UTF-8");
+	}
+	
+	@SuppressWarnings("restriction")
+	public static String cryptage(String str) throws Exception{
+        SecretKey key = new SecretKeySpec("P@ssroot$/240204".getBytes(), "AES");
+		Cipher ecipher = Cipher.getInstance("AES");
+		ecipher.init(Cipher.ENCRYPT_MODE, key);
+		 byte[] utf8 = str.getBytes("UTF-8");
+         byte[] enc = ecipher.doFinal(utf8);
+         return new sun.misc.BASE64Encoder().encode(enc);
+	}
+	
+	
+	public static void main(String[] args) {
+		try {
+		System.out.println(cryptage("AKIAICDVXF4E4DTBDMOQ"));
+		System.out.println(decryptage("ohH75ouRJZU9K+9mqh4o5u7+radU40gkUfcccVvc920="));
+		
+		} catch(Exception e){
+			System.out.println(e.getMessage());
+			System.out.println(e.getCause());
+		}
+		
 	}
 
 }
